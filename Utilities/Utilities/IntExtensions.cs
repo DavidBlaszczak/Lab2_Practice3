165bf4704d98355a2ed7180bb2b9351b9fabc539
DavidBlaszczak <blaszczakdavid26@gmail.com>
2025-06-16 18:49:27 +0200

    Solutions

--- Utilities/Utilities/IntExtensions.cs
+++ Utilities/Utilities/IntExtensions.cs
@@ -2,5 +2,20 @@
 
 public static class IntExtensions
 {
-    //your code goes here
+    public static string ToOrdinal(this int number)
+    {
+        var n = number % 100;
+        if (n >= 11 && n <= 13) return number + "th";
+        return number + (number % 10 == 1 ? "st" : number % 10 == 2 ? "nd" : number % 10 == 3 ? "rd" : "th");
+    }
+
+    public static bool IsPrime(this int number)
+    {
+        if (number < 2) return false;
+        for (int i = 2; i * i <= number; i++)
+        {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
 }
--- Utilities/Utilities/StringExtensions.cs
+++ Utilities/Utilities/StringExtensions.cs
@@ -2,5 +2,10 @@
 
 public static class StringExtensions
 {
-    //your code goes here
+    public static string ToSnakeCase(this string input)
+    {
+        if (string.IsNullOrWhiteSpace(input)) return "";
+        var clean = new string(input.Where(c => char.IsLetterOrDigit(c) || char.IsWhiteSpace(c)).ToArray());
+        return string.Join("_", clean.ToLower().Split(' ', StringSplitOptions.RemoveEmptyEntries));
+    }
 }
